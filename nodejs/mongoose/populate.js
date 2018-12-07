64bf4141ca68fa279b4a41d252f13ecfedfda18f
Hayata Takagi <kozamurai882@gmail.com>
2018-12-07 19:23:08 +0900

    スタックエラー解決途中

--- nodejs/mongoose/populate.js
+++ nodejs/mongoose/populate.js
@@ -2,34 +2,15 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/nodedb', { useNewUrlParser: true });
 var db = mongoose.connection;
 var Schema = mongoose.Schema;
-var ObjectId = mongoose.Types.ObjectId;
+var ObjectId = Schema.Types.ObjectId;
+const START_MS = new Date().getTime();
 
 db.on('error', console.error.bind(console, 'connection error:'));
 
 
 
 db.once('open', function() {
-  // console.log("we're connected!");
-
-  // const schema = schemaBuilder();
-  // const schemaList[schema.name] = schema;
-
-  // スキーマ定義
-  // TODO: スキーマ作成の自動化
-  // const schemaList = {
-  //   "personSchema": Schema({
-  //     _id: Schema.Types.ObjectId,
-  //     name: String,
-  //     age: Number,
-  //     stories: [{ type: Schema.Types.ObjectId, ref: 'Story' }]
-  //   }),
-  //   "storySchema": Schema({
-  //     _id: Schema.Types.ObjectId,
-  //     author: { type: Schema.Types.ObjectId, ref: 'Person' },
-  //     title: String,
-  //     fans: [{ type: Schema.Types.ObjectId, ref: 'Person' }]
-  //   }),
-  // };
+showLog('Query Start.');
 
   const schemaSeeds = {
     "personSchema": {
@@ -45,47 +26,47 @@ db.once('open', function() {
     },
   };
 
-  const schemaList = {
-    "personSchema": Schema(schemaSeeds['personSchema']),
-    "storySchema": Schema(schemaSeeds['personSchema'])
+  // const schemaSeeds2 = {
+  //   "storySchema": {
+  //     _id: Schema.Types.ObjectId,
+  //     author: { type: Schema.Types.ObjectId, ref: 'Person' },
+  //     title: String,
+  //     fans: [{ type: Schema.Types.ObjectId, ref: 'Person' }]
+  //   },
+  // };
+
+  const schemaSeeds2 = {
+    "personSchema": {
+      _id: Schema.Types.ObjectId,
+      name: String,
+      age: Number,
+      stories: [{ type: Schema.Types.ObjectId, ref: 'Story' }]
+    },
   };
 
 
-
+  // オリジナルスキーマの作成
+  const schemaList = {};
+  schemaBilder(schemaSeeds, schemaList);
 
   // mvスキーマの定義
-  let mvschemaList = [];
-  Object.keys(schemaList).forEach(function(value) {
-    mvschemaList[value] = mvSchemaBilder(schemaList[value]);
-  });
-  let mvschemaList2 = {
-    "storySchema": Schema({
-      _id: Schema.Types.ObjectId,
-      author: {
-        _id: Schema.Types.ObjectId,
-        name: String,
-        age: Number,
-        stories: [{ type: Schema.Types.ObjectId, ref: 'Story' }]},
-      title: String,
-      fans: [{
-        _id: Schema.Types.ObjectId,
-        name: String,
-        age: Number,
-        stories: [{ type: Schema.Types.ObjectId, ref: 'Story' }]}]
-    }),
-  };
-  // let testPersonSchema = Schema(mvschemaList['storySchema'].obj);
-  // console.log(mvschemaList['storySchema'].obj);
-
-  // console.log('[START]LIST!!!!!');
+  let mvschemaList = {};
+  mvSchemaBilder(schemaSeeds2, mvschemaList);
+  // Object.keys(schemaSeeds).forEach(function(value) {
+  //   showLog(value);
+  //   mvschemaSeeds[value] = Schema(mvSchemaBilder(schemaSeeds[value]));
+  // });
+  // // TODO: ここでスタックオーバーが起こる
+  // mvschemaSeeds['personSchema'] = Object.assign({}, JSON.parse(JSON.stringify(schemaSeeds['personSchema'])));
+  // mvSchemaBilder(mvschemaSeeds['personSchema']);
+  // mvschemaList['personSchema'] = Schema(mvschemaSeeds['personSchema']);
+  //
+  // mvschemaSeeds['storySchema'] = Object.assign({}, JSON.parse(JSON.stringify(schemaSeeds['storySchema'])));
+  // mvSchemaBilder(mvschemaSeeds['storySchema']);
+  // mvschemaList['storySchema'] = Schema(mvschemaSeeds['storySchema']);
   // console.log(mvschemaList['storySchema']);
-  // console.log('[END]LIST!!!!!');
-  // console.log('[START]LIST222!!!!!');
-  // console.log(mvschemaList2['storySchema']);
-  // console.log('[END]LIST222!!!!!');
-  // console.log('[START]LIST333!!!!!');
-  // console.log(testPersonSchema);
-  // console.log('[END]LIST333!!!!!');
+
+
   // プリフックの定義
   // Object.keys(schemaList).forEach(function(value) {
   //   schemaList[value].pre('findOne', function(next) {
@@ -109,16 +90,17 @@ db.once('open', function() {
   // TODO: モデル定義の自動化
   var Story = mongoose.model('Story', schemaList['storySchema']);
   var Person = mongoose.model('Person', schemaList['personSchema']);
-  var MvStory = mongoose.model('MvStory', mvschemaList['storySchema']);
+  // var MvStory = mongoose.model('MvStory', mvschemaList['storySchema']);
+  // var MvPerson = mongoose.model('MvPerson', mvschemaList['personSchema']);
 
   // クエリ作成
-  Story.
-  findOne({ title: 'Sotsuken' }).
-  populate('author').
-  exec(function (err, story) {
-    if (err) return console.log(err);
-    console.log(story);
-  });
+  // Story.
+  // findOne({ title: 'Sotsuken' }).
+  // populate('author').
+  // exec(function (err, story) {
+  //   if (err) return console.log(err);
+  //   console.log(story);
+  // });
 
   // MvStory.
   // findOne({ title: 'Sotsuken_mv' }).
@@ -152,23 +134,36 @@ db.once('open', function() {
     return true;
   }
 
-  // 与えられたスキーマのMVスキーマを作成
-  function mvSchemaBilder(schema) {
-    replaceRefSchema(schema.obj);
-    return schema;
+  // Seedsからスキーマを作成する
+  function schemaBilder(seedObjects, schemaObjects) {
+    Object.keys(seedObjects).forEach(function(value) {
+        schemaObjects[value] = Schema(seedObjects[value]);
+    });
+  }
+
+  // 与えられたSeedsのMVスキーマを作成
+  function mvSchemaBilder(originalSeedObjects, mvSchemaObjects) {
+    let mvschemaSeeds = {};
+    Object.keys(originalSeedObjects).forEach(function(value) {
+      mvschemaSeeds[value] = completeAssign({}, schemaSeeds[value]);
+      replaceRefSchema(mvschemaSeeds[value]);
+      mvSchemaObjects[value] = Schema(mvschemaSeeds[value]);
+      console.log(mvschemaSeeds[value]);
+    });
   }
 
   // refがあるスキーマをref先のスキーマに置き換える
   // ref先のrefは置き換えない
   function replaceRefSchema(obj) {
-    Object.keys(obj).forEach(function(value, key) {
+    Object.keys(obj).forEach(function(value) {
       if (typeof obj[value] != "object") {
         // String, Number, _id 等を弾く
         return;
       } else {
         if(obj[value].hasOwnProperty('ref')) {
           // 参照型の場合埋め込み型に書き換える
-          obj[value] = schemaList[getSchemaName(obj[value].ref)].obj;
+          // console.log(schemaSeeds['personSchema']);
+          obj[value] = completeAssign({}, schemaSeeds[getSchemaName(obj[value].ref)]);
         } else {
           // 探索を続ける
           replaceRefSchema(obj[value]);
@@ -181,4 +176,47 @@ db.once('open', function() {
   function getSchemaName(model_name) {
     return model_name.toLowerCase() + 'Schema';
   }
+
+  // 経過時間,呼び出し元メソッド,メッセージをログ表示する
+  function showLog(msg) {
+    let now_time = new Date(),
+        elapsed_time = (now_time.getTime() - START_MS) / 1000;
+    if (showLog.caller.name) {
+      console.log('[' + getNowTime() + '|' + elapsed_time + ']'  + showLog.caller.name + ' | ' + msg);
+    } else {
+      console.log('[' + getNowTime() + '|' + elapsed_time + ']' + msg);
+    }
+  }
+
+  // 現在時刻をHH:mm:SSで返す
+  function getNowTime() {
+    let now_time = new Date(),
+        now_hour = now_time.getHours() + 9 ,
+        now_minute = now_time.getMinutes(),
+        now_second = now_time.getSeconds();
+    return ('0'+now_hour).slice(-2) + ':' + ('0'+now_minute).slice(-2) + ':' + ('0'+now_second).slice(-2);
+
+  }
+
+  // 記述子を完全にコピーする代入関数
+function completeAssign(target, ...sources) {
+  showLog(completeAssign.caller.name);
+  sources.forEach(source => {
+    let descriptors = Object.keys(source).reduce((descriptors, key) => {
+      descriptors[key] = Object.getOwnPropertyDescriptor(source, key);
+      return descriptors;
+    }, {});
+    // // デフォルトで、Object.assign は列挙可能なシンボルもコピーします。
+    Object.getOwnPropertySymbols(source).forEach(sym => {
+      let descriptor = Object.getOwnPropertyDescriptor(source, sym);
+      if (descriptor.enumerable) {
+        descriptors[sym] = descriptor;
+      }
+    });
+    Object.defineProperties(target, descriptors);
+  });
+  return target;
+}
+
+  showLog('Query End.');
 });
